3c1361174a9d82fd6bb401a184396e5ec1b99d51
surdu <postal2600@gmail.com>
2017-06-07 22:04:50 +0300

    Improved the add-member entries
    Fixes #1

--- assets/js/addPicture.js
+++ assets/js/addPicture.js
@@ -14,14 +14,23 @@ chrome.storage.sync.get({iconSize: 30}, function(options) {
 		observer.observe(newsNode, {childList: true});
 	}
 
+	function hasClass(node, className) {
+		return node.className.indexOf(className) !== -1;
+	}
 
 	function addPictureToNodes(nodes) {
 		for (var f = 0; f < nodes.length; f++) {
+
 			var node = nodes[f];
 
-			if (node.className && node.className.indexOf("simple") !== -1) {
-				var entry = node.querySelector("a");
-				var username = entry.innerHTML;
+			if (node.className && hasClass(node, "simple")) {
+				var links = node.querySelectorAll("a");
+				var userLink = links[0];
+				var isAddEntry = hasClass(node, "member_add");
+				if (isAddEntry) {
+					userLink = links[1];
+				}
+				var username = userLink.innerHTML;
 
 				var avatarImg = document.createElement("img");
 				avatarImg.setAttribute("src", `//github.com/${username}.png?size=${options.iconSize * 2}`);
@@ -29,7 +38,25 @@ chrome.storage.sync.get({iconSize: 30}, function(options) {
 				avatarImg.setAttribute("height", options.iconSize);
 				avatarImg.className = "userPicture";
 
-				entry.prepend(avatarImg);
+				var avatarLink = document.createElement("a");
+				avatarLink.setAttribute("href", userLink.getAttribute("href"));
+				avatarLink.appendChild(avatarImg);
+
+				var title = node.querySelector(".title");
+				if (isAddEntry) {
+					// for better readability, we'll rephrase the entry
+					// from 'X added Y to REPO' into 'Y was added by X to REPO'
+					title.innerHTML = "";
+					title.appendChild(avatarLink);
+					title.appendChild(links[1]);
+					title.appendChild(document.createTextNode(" was added by "));
+					title.appendChild(links[0]);
+					title.appendChild(document.createTextNode(" to "));
+					title.appendChild(links[2]);
+				}
+				else {
+					title.prepend(avatarLink);
+				}
 			}
 		}
 	}
